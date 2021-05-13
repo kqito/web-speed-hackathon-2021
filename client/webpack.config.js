72027f5abca4d540ea7b1c06ec924793f5041105
kqito <29191111+kqito@users.noreply.github.com>
2021-05-13 18:18:14 +0900

    ひとまずdeploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,6 @@ const config = {
       '/api': 'http://localhost:3000',
     },
   },
-  devtool: 'inline-source-map',
   entry: {
     main: [
       'core-js',
@@ -31,7 +30,7 @@ const config = {
       path.resolve(SRC_PATH, './index.jsx'),
     ],
   },
-  mode: 'none',
+  mode: 'production',
   module: {
     rules: [
       {
